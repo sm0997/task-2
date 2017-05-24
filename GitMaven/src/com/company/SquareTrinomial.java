e817047029341adf51225d2250d47b8b1cb741e8
Zharkova Ilona <sm0997@mail.ru>
2017-05-24 18:56:50 +0600

    2commit

---
+++ GitMaven/src/com/company/SquareTrinomial.java
@@ -0,0 +1,43 @@
+package com.company;
+
+public class SquareTrinomial {
+    private double a;
+    private double b;
+    private double c;
+
+    public SquareTrinomial() {  }
+
+    public SquareTrinomial(double a,double b,double c){
+        this.a=a;
+        this.b=b;
+        this.c=c;
+    }
+
+    public double getA() {
+        return a;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public double getC() {
+        return c;
+    }
+
+    public void setA(double a) {
+        this.a = a;
+    }
+
+    public void setB(double b) {
+        this.b = b;
+    }
+
+    public void setC(double c) {
+        this.c = c;
+    }
+
+    public double [] solveST(){
+        if
+    }
+}
